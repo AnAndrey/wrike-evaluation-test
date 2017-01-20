6cf1200879b964a397a92363ed7572b689ec3450
AnAndrey <an.andrey.g@gmail.com>
2017-01-20 18:00:00 +0300

    Test webapp.

---
+++ web/main.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2017, admin. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/platform/browser.dart';
+
+import 'package:wrike_evaluation_test/app_component.dart';
+
+main() {
+  bootstrap(AppComponent);
+}
